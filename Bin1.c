519dc8f80bd6280c1d031f543407399503b59d5b
Jason Melvin Hartono <jason.melvin689@gmail.com>
2023-12-25 14:21:40 +0700

    added features such as random generation of rooms

--- Bin1.c
+++ Bin1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <ncurses.h>
-#include <windows.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
-char **map;
+bool pplaced = 0;
+bool rplaced = 0;
+int rsy, rsx;
+int ry, rx;
 
 void initmap(char ***m, int row, int col){
 	*m = (char **)malloc(sizeof(char *) * row);
@@ -12,33 +16,65 @@ void initmap(char ***m, int row, int col){
 	}
 }
 
-int dungeon(int row, int col){
-	for(int yy = 0; yy < row; yy++){
-		for(int xx = 0; xx < col; xx++){
-			map[yy][xx] = '#';
-			mvaddch(yy, xx, '#');
+int dungeon(int row, int col, char **map, int * const x, int * const y, char c){
+	
+	srand(time(NULL));
+	
+	if(!rplaced){
+		for(int yy = 0; yy < row; yy++){
+			for(int xx = 0; xx < col; xx++){
+				map[yy][xx] = '#';
+			}
 		}
 	}
 	
-	for(int yy = 11; yy < row/2+10; yy++){
-		for(int xx = 11; xx < col/2+10; xx++){
-			map[yy][xx] = ' ';
-			mvaddch(yy, xx, ' ');
+	for(int yy = 0; yy < row; yy++){
+		for(int xx = 0; xx < col; xx++){
+			if(map[yy][xx] == ' ') mvaddch(yy, xx, ' ');
+			else mvaddch(yy, xx, '#');
 		}
 	}
+	
+	if(!rplaced){
+		
+		// make room point
+		do{
+			ry = rand() % row - 3;
+			rx = rand() % col - 3;
+		}while(ry < 1 || rx < 1);
+		
+		// generate size
+		do{
+			rsy = rand() % 11 + 1;
+			rsx = rand() % 22 + 1;
+		} while(ry + rsy > row-2 || rx + rsx > col-2);
+		
+		for(int yy = ry; yy < ry+rsy; yy++){
+			for(int xx = rx; xx < rx+rsx; xx++){
+				map[yy][xx] = ' ';
+				mvaddch(yy, xx, ' ');
+			}
+		}
+		rplaced =1;
+	}
+	
+	if(!pplaced){
+		do{
+			*y = rand() % row;
+			*x = rand() % col;
+		} while(map[*y][*x] != ' ');
+		pplaced = 1;
+	}
+	
+	if(c == 'w' &&  map[(*y)-1][*x] == ' ') (*y)--;
+	else if(c == 's' && map[(*y)+1][*x] == ' ') (*y)++;
+	else if(c == 'a' && map[*y][(*x)-1] == ' ') (*x)--;
+	else if(c == 'd' && map[*y][(*x)+1] == ' ') (*x)++;
+	
+	mvaddch(*y, *x, '@'); //print the character to the screen with a spesific x and y coordinates
 }
 
-int movement(char c, int * const x, int * const y){ // a constant pointer to a changable value if (const int *p) it makes the value inside the pointer const not the pointer it self
-	if(c == 'w' &&  map[(*y)-1][*x] == ' ') {
-	    (*y)--;
-	} else if(c == 's' && map[(*y)+1][*x] == ' ') {
-	    (*y)++;
-	} else if(c == 'a' && map[*y][(*x)-1] == ' ') {
-	    (*x)--;
-	} else if(c == 'd' && map[*y][(*x)+1] == ' ') {
-	    (*x)++;
-	}
-}
+//int movement(char c, int * const x, int * const y) // a constant pointer to a changable value if (const int *p) it makes the value inside the pointer const not the pointer it self
 
 int main(){
 
@@ -48,6 +84,7 @@ int main(){
 	char c = '\0';
 	int x = 11, y = 11;
 	int row, col;
+	char **map;
 	
 	
 	initscr(); // initialize the window screen
@@ -65,11 +102,7 @@ int main(){
 	do{
 //		clear(); //for clearing the console (simmilar to the windows system("cls")
 
-		dungeon(row, col); // a function for handling the display of the dungeon
-		
-		movement(c, &x, &y);
-		
-		mvaddch(y, x, '@'); //print the character to the screen with a spesific x and y coordinates
+		dungeon(row, col, map, &x, &y, c); // a function for handling the display of the dungeon
 
 	}while((c = getch()) != 27); // 27 == escape button
 	
